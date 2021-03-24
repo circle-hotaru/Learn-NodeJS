166ebad30b56f5125ebc2609636c3a02549f1454
circlehotarux <mightyherox@gmail.com>
2021-03-24 21:05:53 +0800

    feat: add & init blog-express

---
+++ blog-express/routes/blog.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+const { getList,
+    getDetail,
+    newBlog,
+    updateBlog,
+    delBlog
+} = require('../controller/blog')
+const { SuccessModel, ErrorModel } = require('../model/resModel')
+
+router.get('/list', function (req, res, next) {
+    let author = req.query.author || ''
+    const keyword = req.query.keyword || ''
+
+    if (req.query.isadmin) {
+        // // 管理员界面
+        // const loginCheckResult = loginCheck(req)
+        // if (loginCheckResult) {
+        //     // 未登录
+        //     return loginCheckResult
+        // }
+        // // 强制查询自己的博客
+        author = req.session.username
+    }
+
+    const result = getList(author, keyword)
+    return result.then(listData => {
+        res.json(new SuccessModel(listData))
+    })
+});
+
+router.get('/detail', function (req, res, next) {
+    res.json({
+        errno: 0,
+        data: 'OK'
+    })
+});
+
+module.exports = router;
